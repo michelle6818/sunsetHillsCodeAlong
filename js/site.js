96f03971ad768f041a6e8ac0105eea117fc47ad1
michelle6818 <75517393+michelle6818@users.noreply.github.com>
2021-01-14 12:41:03 -0500

    changes

--- js/site.js
+++ js/site.js
@@ -1,46 +1,93 @@
 
 
 document.getElementById("btnJoin").addEventListener("click", Join);
+document.getElementById("btnSort").addEventListener("click", SortAscending);
+document.getElementById("btnSortDesc").addEventListener("click", SortDescending);
+document.getElementById("btnSwitch").addEventListener("click", DOWSwitch);
 
 
-// JOIN function
-
-function Join(){
-    let fruits = ["Banana", "Orange", "Apple", "Mango"];
-    // OR....
-    // let fruit = new Array();
-    // fruits.push("banana");
-    // fruits.push("orange");
-    // fruits.push("apple");
-    // fruits.push("mango");
-
-    // uses a specific join character
-    document.getElementById("outputCard1").innerHTML =  fruits.join("*");
-    // this way will use comma by default
-    // document.getElementById("outputCard1").innerHTML =  fruits;
+function DOWSwitch(){
+    let day;
+    // let DaysOfWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    // document.getElementById("outputCard7").innerText = DaysOfWeek[new Date().getDay()]
+            
+    switch (new Date().getDay()) {
+      case 0:
+        day = "Sunday";
+        break;
+      case 1:
+        day = "Monday";
+        break;
+      case 2:
+        day = "Tuesday";
+        break;
+      case 3:
+        day = "Wednesday";
+        break;
+      case 4:
+        day = "Thursday";
+        break;
+      case 5:
+        day = "Friday";
+        break;
+      case 6:
+        day = "Saturday";
+      default:
+        day = "Unknown";
+    }
+    
+    document.getElementById("outputCard7").innerText = `Today is ${day}`;      
 }
 
-// Reverse
+
+function SortAscending(){
+    let array = [40, 100, 1, 5, 25, 10];
+    document.getElementById("outputCard5").innerText = 
+        array.sort(function(a, b){return a - b}).join(" < ");
+}
+
+function SortDescending(){
+    let array = [40, 100, 1, 5, 25, 10];
+    document.getElementById("outputCard6").innerText = 
+        array.sort(function(a, b){return b - a}).join(" > ");
+}
+
+
+function Join()
+{
+    let fruits = ["Banana", "Orange", "Apple", "Mango"];
+    // let fruits = new Array();
+    // fruits.push("Banana");
+    // fruits.push("Orange");
+    // fruits.push("Apple");
+    // fruits.push("Mango");
+
+    // document.getElementById("outputCard1").innerHTML = fruits.join(" --> ");
+    document.getElementById("outputCard1").innerHTML = fruits;
+
+}
+
 function Reverse(){
-    
+
     let fruits = ["Banana", "Orange", "Apple", "Mango"];
-    // let reverseFruits = fruits.reverse()
-    // document.getElementById("outputCard2").innerHTML = reverseFruits.join(" * ");
-    document.getElementById("outputCard2").innerHTML = fruits.reverse();
+    let reverseFruits = fruits.reverse();
+    document.getElementById("outputCard2").innerHTML = reverseFruits.join(" * ");
+  //document.getElementById("outputCard1").innerHTML = fruits;
+
+
+
 }
 
-function ReverseUsingSplitandJoin(){
-    let word = "Hello";
-    // go from string to array
-    // step 1: split the word Hello into an Array --> ['H','e','l','l','o']
-    // step 2: reverse the array --> ['o','l','l','e','H']
-    // step 3: join reverse array without a join character --> "olleH"
-    document.getElementById("outputCard3").innerText =
+function ReverseUsingSplitAndJoin(){
+    let word = "Hello";       
+    //Step 1: Split the word Hello into an Array --> word.split('') = ['H','e','l','l','o'] 
+    //Step 2: Reverse the array --> ['o','l','l','e','H']
+    //Step 3: Joinm the reversed array without a join character --> "olleH"
+    document.getElementById("outputCard3").innerText = 
         word.split('').reverse().join('');
-        // will split, then reverse, then join
-    
 }
+
 function Sort(){
-    let array = ["Bananas", "Oranges", "Apples", "Mangos"];
+    let array = ["Banana", "Orange", "Apple", "Mango"];
     document.getElementById("outputCard4").innerText = array.sort();
 }
